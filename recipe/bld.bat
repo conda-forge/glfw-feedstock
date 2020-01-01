ea131568218dda7a0dbe37b2d787b6e0b151d209
Mark Harfouche <mark.harfouche@gmail.com>
2020-01-01 16:29:39 -0500

    build with ninja

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,7 @@
 mkdir build
 cd build
 
-cmake  -G "%CMAKE_GENERATOR%"        ^
+cmake -LAH -G "Ninja"                ^
     -DCMAKE_PREFIX_PATH=%PREFIX%     ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DBUILD_SHARED_LIBS=ON           ^
